845f4970087f64dfa3bd7a7b7f7479f8267b2146
Douglas Modena <d.modena@live.com>
2019-08-25 02:57:51 -0300

    Returning TodoItem from API.

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TodoApi.Models
 {
+    [Table(name:"todoitem")]
     public class TodoItem
     {
+        [Column(name:"id")]
         public long Id { get; set; }
+        [Column(name:"name")]
         public string Name { get; set; }
+        [Column(name:"iscomplete")]
         public bool IsComplete { get; set; }
     }
 }
